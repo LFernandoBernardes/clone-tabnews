b55f17fb6e36175989dc8d16d7849c0177dfc162
Luiz Fernando Bernardes Borges <164089467+LFernandoBernardes@users.noreply.github.com>
2025-11-04 22:10:13 +0000

    correção da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return<h1>Cassia, te amo, tu és muito espacial para mim. De uma risadinha se tu gostou desta mensagem </h1>
+    return<h1>Cassia, te amo!! 
+        Tu és muito especial para mim. De uma risadinha se tu gostou desta mensagem </h1>
     return<h1>Eu amo você</h1>
 }
 
