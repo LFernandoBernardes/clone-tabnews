bc70d2d0885d223c9807af1cffad74a356418fc4
Luiz Fernando Bernardes Borges <164089467+LFernandoBernardes@users.noreply.github.com>
2025-11-04 22:04:10 +0000

    atualiza a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
-    return<h1>Teste - Data 10 de outubro </h1>
+    return<h1>Cassia, te amo, tu és muito espacial para mim. De uma risadinha se tu gostou desta mensagem </h1>
     return<h1>Eu amo você</h1>
 }
 
-export default Home;
+export default Home; 
