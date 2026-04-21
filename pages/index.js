4baf57277932d7a21e6c380bc61443fac7adb95e
Luiz Fernando Bernardes Borges <164089467+LFernandoBernardes@users.noreply.github.com>
2026-04-21 15:05:00 +0000

    refina arquivos `database.js` e `.env.development`

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Cassia, te amo!! Tu és muito especial para mim. De uma risadinha se tu
-      gostou desta mensagem{" "}
+      gostou desta mensagem
     </h1>
   );
   return <h1>Eu amo você</h1>;
