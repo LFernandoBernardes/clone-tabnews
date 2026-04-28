c0980bcc6937e0f2e466af95b11de30b53c8ed3b
Luiz Fernando Bernardes Borges <164089467+LFernandoBernardes@users.noreply.github.com>
2026-04-28 22:46:36 +0000

    inserir informações de dependencias no `index.js` testes tdd

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,21 @@
 import database from "infra/database.js";
+import { version } from "react";
 
+// Retornar data de agora no 'body' do json da url http://localhost:3000/api/v1/status
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
+// Testar o retorno da API '/api/v1/status'
 test("GET to/api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // teste para ver o body 'texto' do que esta rodando na url acima
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  // teste espera que a propridade updated_at esteja definido, ou seja existe.
+  expect(responseBody.updated_at).toBeDefined();
+
+  //Verifica que o valor do updated_at é a data now esperada
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  // Verifica version do Postgres
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
 });
