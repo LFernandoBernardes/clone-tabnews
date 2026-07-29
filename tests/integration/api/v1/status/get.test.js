be802ea74fbe048c2cad7db27329d16f850ad5ef
Luiz Fernando Bernardes Borges <164089467+LFernandoBernardes@users.noreply.github.com>
2026-07-29 00:15:13 +0000

    Database Opened Connections

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -16,4 +16,8 @@ test("GET to/api/v1/status should return 200", async () => {
 
   // Verifica version do Postgres
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  // Verifica Conexões com o Banco Postegres
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(2);
 });
